3ae9b317843a35e422a851483449d5f29e05deb8
Danny Bushkanets <d.bushkanets@gmail.com>
2022-03-30 14:27:15 -0700

    make migration and add videos to admin

--- backend/videos/admin.py
+++ backend/videos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from videos.models.collection import Collection
+from videos.models.video import Video
 
 # Register your models here.
+admin.site.register(Collection)
+admin.site.register(Video)
